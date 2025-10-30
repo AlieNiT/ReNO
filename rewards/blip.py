f57110a88b18847a904be04d79a0375daa317520
AlieNiT <ali.blade.1380@gmail.com>
2025-10-30 18:57:45 +0330

    Just trying to make it work

--- rewards/blip.py
+++ rewards/blip.py
@@ -32,33 +32,21 @@ class BLIPLoss(BaseRewardLoss):
         super().__init__("BLIP", weighting)
 
     def get_image_features(self, image: torch.Tensor) -> torch.Tensor:
-        vision_outputs = self.blip_model.vision_model(pixel_values=image)
-        image_embeds = vision_outputs[0]
-        image_features = self.blip_model.vision_proj(image_embeds[:, 0, :])
-        image_features = torch.nn.functional.normalize(image_features, dim=-1)
-        return image_features
+        return image
 
     def get_text_features(self, prompt: str) -> torch.Tensor:
-        prompt_token = self.processor(
-            text=[prompt], padding=True, return_tensors="pt"
-        ).to(self.blip_model.device)
-        
-        text_outputs = self.blip_model.text_encoder(
-            input_ids=prompt_token.input_ids,
-            attention_mask=prompt_token.attention_mask,
-            return_dict=True,
-        )
-        question_embeds = text_outputs.last_hidden_state
-        text_features = self.blip_model.text_proj(question_embeds[:, 0, :])
-        text_features = torch.nn.functional.normalize(text_features, dim=-1)
-        return text_features
+        return prompt
+    
+    def process_features(self, features):
+        return features
 
     def compute_loss(
         self, image_features: torch.Tensor, text_features: torch.Tensor
     ) -> torch.Tensor:
-        blip_loss = (
-            100
-            - (image_features @ text_features.T).mean()
-            * self.blip_model.logit_scale.exp()
-        )
-        return blip_loss
+        text_inputs = self.processor(text=text_features, return_tensors="pt")
+        blip_loss = self.blip_model(
+            pixel_values=image_features, 
+            **text_inputs, 
+            use_itm_head=False
+        )[0]
+        return 1 - blip_loss
